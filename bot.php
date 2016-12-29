b44613cb033867f8b6c95f5c44e16cc0807bc666
aodloso <winniepooh_aod@hotmail.com>
2016-12-29 18:20:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,9 +19,9 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			
 			$messages = [
-				'type' => 'image',
-				'originalContentUrl' => 'http://clinic.matthailand.com/user3.jpg',
-				'previewImageUrl' => 'http://clinic.matthailand.com/user3.jpg'
+				 "type": "sticker",
+				  "packageId": "1",
+				  "stickerId": "1"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
