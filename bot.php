97a144957b8429a65916ad0b6c86f10f59d3ebeb
aodloso <winniepooh_aod@hotmail.com>
2016-12-29 17:51:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,12 +19,16 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'Hello, user'
+			],
+			[
+				'type' => 'text',
+				'text' => 'May I help you?'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
-			$messages = "5555";
+			
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
