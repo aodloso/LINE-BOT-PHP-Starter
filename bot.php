c557e088e37f1779546a7c0f41e77f2fdf5d1f14
aodloso <winniepooh_aod@hotmail.com>
2016-12-29 18:29:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,12 +17,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			
+			if($text == "1+1"){
 			$messages = [
 				 "type" => "sticker",
 				  "packageId" => "1",
 				  "stickerId" => "1"
-			];
+			];}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
