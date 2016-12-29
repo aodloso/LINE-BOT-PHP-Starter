4d22f6517758c147573e682ea106e2a127483cf5
aodloso <winniepooh_aod@hotmail.com>
2016-12-29 17:45:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,6 +24,7 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
+			$messages = "5555";
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
